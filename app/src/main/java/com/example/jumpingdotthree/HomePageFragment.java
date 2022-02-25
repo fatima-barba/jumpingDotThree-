626ffe90aad9e19ded7978e7028e5fc87da4a9e7
fatima <fatimaa@vandenrobotics.com>
2022-02-24 16:28:34 -0800

    started home page
    home page started

--- app/src/main/java/com/example/jumpingdotthree/HomePageFragment.java
+++ app/src/main/java/com/example/jumpingdotthree/HomePageFragment.java
@@ -3,6 +3,7 @@ package com.example.jumpingdotthree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class HomePageFragment extends AppCompatActivity {
 
